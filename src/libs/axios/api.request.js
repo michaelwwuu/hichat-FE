9c74bec2a7c177269d7deec7bfaa6e4b4a5854e1
Michael Wu <98412060@stud.sju.edu.tw>
2022-05-19 12:18:43 +0800

    remove

--- src/libs/axios/api.request.js
+++
@@ -1,4 +0,0 @@
-import HttpRequest from '../axios/axios.js'
-
-var baseUrl = process.env.NODE_ENV === 'test' ? '/' : process.env.NODE_ENV === 'pre' ? '/' : '/';
-export const axios = new HttpRequest(baseUrl)
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import ws from "./ws";
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  modules: { ws }
-})
