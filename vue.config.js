09d38d384cfbf0a719956997a648e0a615bad947
Michael Wu <98412060@stud.sju.edu.tw>
2022-08-29 01:16:27 +0800

    # 2022.08.29 設置打包自動移除 console.log

--- vue.config.js
+++ vue.config.js
@@ -26,4 +26,22 @@ module.exports = {
       }
     }
   },
+  chainWebpack: (config) => {
+    // 去掉debugger console
+    config.when(process.env.NODE_ENV !== "development", config => {
+      config.optimization.minimizer('terser').tap((args) => {
+        // 注释console.*
+        args[0].terserOptions.compress.drop_console = true
+        // remove debugger
+        args[0].terserOptions.compress.drop_debugger = true
+        // 移除 console.log
+        args[0].terserOptions.compress.pure_funcs = ['console.log']
+        // 去掉注释 如果需要看chunk-vendors公共部分插件，可以注释掉就可以看到注释了
+        args[0].terserOptions.output = {
+          comments: false
+        };
+        return args
+      })
+    })
+  }
 };
